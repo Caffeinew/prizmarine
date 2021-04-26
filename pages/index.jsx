c8377e02cce99ef1dbec4c6818c3a3fe7e9ab4f4
Caffeinew <tarabrin181@mail.ru>
2021-04-26 06:12:57 +0300

    smm

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,10 +3,15 @@ import Flow from "../components/flow";
 import Footer from "../components/footer";
 import Landing from "../components/landing";
 import Services from "../components/services";
+import Head from 'next/head'
 
 export default function Index() {
   return (
     <>
+    <Head>
+     <title>Prizmarine - digital agency</title>
+     <meta name="description" content="Призмарин - Разработка сайтов. Создание графического и веб дизайна, SMM, продвижение в социальных сетях, брендирование" />
+    </Head>
       <Flow />
       <Landing />
       <About />
