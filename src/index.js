37e9f1ae07ef9fb4d3a3f8feaca31a878c43326f
Caffeinew <tarabrin181@mail.ru>
2021-06-08 18:48:41 +0300

    yandex

--- src/index.js
+++ src/index.js
@@ -9,8 +9,28 @@ new LocomotiveScroll({
   lerp: 0.05,
 });
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js');
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("service-worker.js");
   });
 }
+
+(function (m, e, t, r, i, k, a) {
+  m[i] =
+    m[i] ||
+    function () {
+      (m[i].a = m[i].a || []).push(arguments);
+    };
+  m[i].l = 1 * new Date();
+  (k = e.createElement(t)),
+    (a = e.getElementsByTagName(t)[0]),
+    (k.async = 1),
+    (k.src = r),
+    a.parentNode.insertBefore(k, a);
+})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(80457868, "init", {
+  clickmap: true,
+  trackLinks: true,
+  accurateTrackBounce: true,
+});
